6e97ecc60c023c7c72b3ae28933ccae10d42caa3
Bikal Chudal <bikalchudal11@gmail.com>
2024-04-25 07:00:55 +0545

    initial commit

---
+++ android/app/src/main/kotlin/com/example/students_e_learning/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.students_e_learning
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/features/login/view/splash_screen.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:students_e_learning/src/core/constants/colors.dart';
+import 'package:students_e_learning/src/features/login/view/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    _next();
+    super.initState();
+  }
+
+  _next() {
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => LoginScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: pColor,
+      body: Center(
+        child: Text(
+          "Learnzilla",
+          style: TextStyle(
+            letterSpacing: 2,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'LeagueSpartan',
+            fontSize: 35,
+          ),
+        ),
+      ),
+    );
+  }
+}
