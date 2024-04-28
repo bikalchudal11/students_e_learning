77a36d0da4cd99a858d3c962d1e557a0b019ad4a
ARACHNOlD <sumit11dhamala@gmail.com>
2024-04-28 17:29:42 +0545

    ChatUI

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:students_e_learning/firebase_options.dart';
+import 'package:students_e_learning/src/features/chat/chat.dart';
 import 'package:students_e_learning/src/features/home/view/home_dashboard.dart';
 import 'package:students_e_learning/src/features/login/view/login_screen.dart';
 import 'package:students_e_learning/src/features/login/view/splash_screen.dart';
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
-      home: HomeDashBoard(),
+      home: ChatPage(),
     );
   }
 }
