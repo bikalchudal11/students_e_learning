70fc6109c14876563255ef3abee4240acee3ba67
ARACHNOlD <sumit11dhamala@gmail.com>
2024-05-03 14:27:22 +0545

    Calender_Done

--- lib/src/features/bottom_navigation/calender/calender.dart
+++ lib/src/features/bottom_navigation/calender/calender.dart
@@ -8,44 +8,41 @@ class CalenderPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 247, 244, 244),
+      appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 247, 244, 244),
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 247, 244, 244),
-          toolbarHeight: 70,
-          title: Text(
-            "Class Calender",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
+        toolbarHeight: 70,
+        title: Text(
+          "Class Calender",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            children: [
-              TableCalendar(
-                firstDay: DateTime.utc(1990, 10, 16),
-                lastDay: DateTime.utc(2030, 3, 14),
-                focusedDay: DateTime.now(),
-              ),
-              SizedBox(height: 15),
-              Container(
-                  height: 100,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white,
-                  ),
-                  child: Column(
-                    children: [
-                      CalenderComponent(
-                        chapterName: 'Organic',
-                        subName: 'Chemestry',
-                        subImgUrl:
-                            'https://th.bing.com/th/id/R.ee2256b1de5b9256a084cbd14beee6bc?rik=fbyd%2bzeuXfUznw&riu=http%3a%2f%2fchoosewashingtonstate.com%2fwp-content%2fuploads%2f2017%2f09%2fchemistry.jpg&ehk=lrKH9jWNXPxRy0hWB1akEV%2fp0%2fKfiRfMd94DoktTrJA%3d&risl=&pid=ImgRaw&r=0',
-                      ),
-                    ],
-                  )),
-            ],
-          ),
-        ));
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            TableCalendar(
+              firstDay: DateTime.utc(1990, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: DateTime.now(),
+            ),
+            SizedBox(height: 15),
+            CalenderComponent(
+              chapterName: 'Organic',
+              subName: 'Chemestry',
+              subImgUrl:
+                  'https://th.bing.com/th/id/R.ee2256b1de5b9256a084cbd14beee6bc?rik=fbyd%2bzeuXfUznw&riu=http%3a%2f%2fchoosewashingtonstate.com%2fwp-content%2fuploads%2f2017%2f09%2fchemistry.jpg&ehk=lrKH9jWNXPxRy0hWB1akEV%2fp0%2fKfiRfMd94DoktTrJA%3d&risl=&pid=ImgRaw&r=0',
+            ),
+            SizedBox(height: 10),
+            CalenderComponent(
+              chapterName: 'Kingdome',
+              subName: 'Biology',
+              subImgUrl:
+                  "https://th.bing.com/th/id/OIP.gKMFNZElRpzigfTLwXnl_AHaHa?rs=1&pid=ImgDetMain",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/src/features/bottom_navigation/calender/calender_course-component.dart
+++ lib/src/features/bottom_navigation/calender/calender_course-component.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CalenderComponent extends StatelessWidget {
   String subName;
@@ -13,63 +14,70 @@ class CalenderComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: 80,
-            width: 80,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              image: DecorationImage(
-                image: NetworkImage(subImgUrl),
+    return Container(
+      height: 100,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 85,
+              width: 85,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                image: DecorationImage(
+                  image: NetworkImage(subImgUrl),
+                ),
               ),
             ),
           ),
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              subName,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 4),
-            RichText(
-              text: TextSpan(
-                style: TextStyle(color: Colors.black),
-                children: <TextSpan>[
-                  TextSpan(
-                    text: 'Chapter 2 :  ',
-                    style: TextStyle(color: Colors.grey, fontSize: 13),
-                  ),
-                  TextSpan(
-                      text: chapterName,
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 68, 66, 66),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 13))
-                ],
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Text(
+                subName,
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
-            ),
-            SizedBox(height: 4),
-            Text(
-              "12:40-1:40 pm",
-              style: TextStyle(color: Colors.grey, fontSize: 9),
-            )
-          ],
-        ),
-        SizedBox(width: 40),
-        Icon(
-          Icons.chevron_right_rounded,
-          color: Colors.grey,
-        ),
-        SizedBox(
-          width: 2,
-        )
-      ],
+              SizedBox(height: 4),
+              RichText(
+                text: TextSpan(
+                  style: TextStyle(color: Colors.black),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'Chapter 2 :  ',
+                      style: TextStyle(color: Colors.grey, fontSize: 13),
+                    ),
+                    TextSpan(
+                        text: chapterName,
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 68, 66, 66),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13))
+                  ],
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                "12:40-1:40 pm",
+                style: TextStyle(color: Colors.grey, fontSize: 9),
+              )
+            ],
+          ),
+          SizedBox(width: 40),
+          Icon(
+            Icons.chevron_right_rounded,
+            color: Colors.grey,
+          ),
+          SizedBox(width: 2),
+        ],
+      ),
     );
   }
 }
